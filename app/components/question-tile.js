bab514729ce033d00772acd5730b74878f8a1e9e
Taylor Grohs <taylorgrohs@gmail.com>
2016-04-07 19:42:18 -0700

    add answer-count helper

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // nameList: Ember.computed('question.answers.length', function(){
+  //   var list = "";
+  //   var answerList = this.get('question');
+  //   console.log(answerList);
+  //   answerList.forEach(function(answer){
+  //     if(list === "") {
+  //       list += "";
+  //     } else {
+  //       list += ", ";
+  //     }
+  //   });
+  //   return list;
+  // }),
   actions: {
     update(question, params) {
       this.sendAction('update', question, params);
