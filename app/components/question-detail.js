d52c1b3a7accfff0543f2e4fa19b09a6a37f7880
Taylor Grohs <taylorgrohs@gmail.com>
2016-04-07 20:25:29 -0700

    add upvote service

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  upvoteCount: Ember.inject.service(),
+
+  actions: {
+    upvote(answer) {
+      this.get('upvoteCount').add(answer);
+    },
+  }
 });
